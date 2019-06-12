c3154e26b83495468d498728382d86ec4c905dd5
Trevor Nagaba <trevornagaba@gmail.com>
2019-06-12 22:00:09 +0300

    Initial model commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+# import warningswarnings.filterwarnings('ignore')%matplotlib inline
+
+train_df=pd.read_csv("data/train.csv")
+train_df.head()
